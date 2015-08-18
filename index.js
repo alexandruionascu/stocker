6e790c99617124e651407e2703871cc223adf22b
Alexandru <alexandruionascu.bd@gmail.com>
2015-08-18 20:07:58 +0300

    Fixed login auth

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 var express = require('express');
 var facebook = require('./fb.js');
 var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var Guid = require('guid');
+
 var FacebookStrategy = require('passport-facebook').Strategy;
+
+
+
+var app = express();
+app.use(session({
+  genid: function(req) {
+    return  // use UUIDs for session IDs
+  },
+  secret: 'keyboard cat'
+}))
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
 passport.use('facebook', new FacebookStrategy({
   clientID        : facebook.fbConfig.appID,
@@ -14,19 +35,30 @@ passport.use('facebook', new FacebookStrategy({
   function(access_token, refresh_token, profile, done) {
       console.log(profile);
       console.log(access_token);
+      var user = {};
+      return done(null, user);
   })
 );
 
+function genuuid() {
+  return Guid.create();
+}
 
 
 
-var app = express();
+
 
 //serve images
 app.use("/images", express.static(__dirname + '/images'));
+//serve css
+app.use("/css", express.static(__dirname + '/css'));
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/dashboard', function (req, res) {
+  res.sendFile(__dirname + '/dashboard.html');
 });
 
 var server = app.listen(3000, function () {
@@ -36,35 +68,17 @@ var server = app.listen(3000, function () {
   console.log('Stocker Server started listening on port 3000');
 });
 
-/*
-// route for facebook authentication and login
-// different scopes while logging in
-app.get('/login/facebook', function() {
-  passport.authenticate('facebook', { scope : 'email' });
-});
-
-// handle the callback after facebook has authenticated the user
-app.get('/login/facebook/callback', function() {
-  passport.authenticate('facebook', {
-    successRedirect : '/home',
-    failureRedirect : '/'
-  });
-});
-*/
 
 
-app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook', passport.authenticate('facebook', {authType: 'reauthenticate'}));
   // GET /auth/facebook/callback
   // Use passport.authenticate() as route middleware to authenticate the
   // request. If authentication fails, the user will be redirected back to the
   // login page. Otherwise, the primary route function function will be called,
   // which, in this example, will redirect the user to the home page.
-  app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-  app.get('/logout', function(req, res){
+app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/', successRedirect: '/dashboard' }));
+
+app.get('/logout', function(req, res){
     req.logout();
     res.redirect('/');
-  });
+});
